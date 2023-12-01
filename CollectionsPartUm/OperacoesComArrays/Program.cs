9ea8cccc9162131caa43d5b5eae21cd29bfd0b53
Stephisson de Oliveira Galvão <stephisson@ciandt.com>
2023-12-01 08:39:16 -0300

    Operações com Arrays.

--- CollectionsPartUm/OperacoesComArrays/Program.cs
+++ CollectionsPartUm/OperacoesComArrays/Program.cs
@@ -78,6 +78,54 @@ namespace OperacoesComArrays
             Imprimir(aulas);
             Console.WriteLine("Forma correta de modificar um elemento.");
 
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine("-----------Operações com Arrays------------");
+            Console.WriteLine("-------------------------------------------");
+            //Procurar um elemento no array, usando um método estático "IndexOf" da classe Array que buscará a primeira ocorrência de uma string,
+            //retornando o índice onde foi encontrado:
+            Console.WriteLine("Aula modelando está no índice " + Array.IndexOf(aulas, aulaModelando));//IndexOf(nome do array, nome da variável)
+            
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine(Array.LastIndexOf(aulas, aulaModelando));//Procurando a ultima ocorrencia do array
+            Console.WriteLine("-------------------------------------------");
+            Array.Reverse(aulas);// comando para inverter a ordem do array, o primeiro vira o último e por ai vai...o método Reverse() funciona
+                                 // como um método idempotente, ou seja, se ele for rodado duas vezes, volta-se ao estado original.
+            Imprimir(aulas);
+            Console.WriteLine("-------------------------------------------");
+
+            Array.Resize(ref aulas, 2); //o método Resize() redimensiona o array por referencia a sua declaração fica (ref nomeDoArray, tamanho)
+            Imprimir(aulas);            //Internamente ele cria outro array do tamanho desejado e muda as referencias dos elementos do antigo para o novo.
+
+            Console.WriteLine(" V V V V V --- Retornando para Size 3 --- V V V V V ");
+            Array.Resize(ref aulas, 3); // o ultimo elemento estará nullo, porque foi perdido a referencia.
+            Imprimir(aulas);
+
+            Console.WriteLine(" V V V V V --- Preenchendo o último elemento do array --- V V V V V ");
+            aulas[aulas.Length - 1] = "Conclusão"; // Para garantir de pegar o ultimo array podemos escrever dessa forma
+            Imprimir(aulas);
+
+            Console.WriteLine(" V V V V V --- Colocando em orgem alfabetica elemento do array --- V V V V V ");
+            Array.Sort(aulas); //Uma vez usado não conseguimos voltar ao normal como no método Reverse()
+            Imprimir(aulas);
+
+            Console.WriteLine(" V V V V V --- Copiando um Array --- V V V V V ");
+            string[] copia = new string[2];
+            Array.Copy(aulas, 1, copia, 0, 2); // (array de origem (aulas), o índice inicial a partir do qual copiaremos
+                                               // o array de aulas (1, que equivale à segunda posição), o nome do array de destino,
+                                               // a posição inicial (índice 0), e a quantidade de elementos que iremos copiar (2)
+            Imprimir(copia);
+
+            Console.WriteLine(" V V V V V --- Clonando um Array --- V V V V V ");
+            string[] clone = aulas.Clone() as string[]; // aulas.Clone(); Dessa forma a IDE irá reclamar que não consegue converter o tipo,
+                                                        // então temos que fazer um cast
+            Imprimir(copia);
+
+            Console.WriteLine(" V V V V V --- Limpando um Array --- V V V V V ");
+            Array.Clear(clone, 1, 2);   // Clear é um metodo estático da classe array,
+                                        // ele é montado da seguinte forma (nomeDoArray, o índice da posição que queremos começar a limpar (1)
+                                        // e o número de elementos que queremos limpar (2),
+            Imprimir(clone);
+
         }   // Colocar um Breakpoint aqui para analisarmos o preenchimento se necessário
 
         private static void Imprimir(string[] aulas)
@@ -94,5 +142,15 @@ namespace OperacoesComArrays
                 Console.WriteLine(aulas[i] + " -> Estamos varrendo os elemenos de aulas e imprimindo item a item na variavel aula");
             }
         }
+
+        //C# suporta vários tipos de arrays:
+
+        //array unidimensional
+        //array multidimensional(array retangular)
+        //array de arrays(arrays "denteados" ou "serrilhados")
+        //Saiba mais sobre declaração e uso de arrays neste tutorial:
+
+        //https://cursos.alura.com.br/course/csharp-collections/task/29649#:~:text=C%23%20suporta%20v%C3%A1rios,Trabalhando%20com%20Arrays
+
     }
 }
